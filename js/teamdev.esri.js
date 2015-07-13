1516c316c43189cca5d8275154545dd6e23114ba
paolo Possanzini <paolo@teamdev.it>
2015-07-13 16:33:26 +0200

    Offline TPK support.

--- js/teamdev.esri.js
+++ js/teamdev.esri.js
@@ -283,40 +283,6 @@ m.directive("esriMap", function ($q, esriRegistry) {
   };
 });
 
-m.directive("offlineTileLayer", function($q, esriRegistry){
-  return {
-    restrict:"E", 
-    require: "^esriMap",
-    replace: true,    
-    scope: {
-      id : "@", 
-      url: "@",
-      isOnLine :"=", 
-      onReady: "&"
-    }, 
-    link:{
-      pre: function(scope, element, attrs, esriMap){
-        var ready = $q.defer();
-        scope.isObjectReady = ready.promise;
-        require(["./libs/offline-tiles-advanced-min.js"], function(){
-          scope.this_layer = new O.esri.Tiles.OfflineTileEnablerLayer(scope.url, function(){
-            console.log("Offline Tile Layer Loaded.");
-            ready.resolve();
-            
-             if (scope.onReady())
-              scope.onReady()(scope.this_layer);
-          }, scope.isOnLine);
-          esriMap.addLayer(scope.this_layer);
-          
-          if(scope.id){
-            scope.this_layer.id = scope.id;
-            esriRegistry.set(scope.id, scope.this_layer);
-          }
-        });
-      }}
-  };
-});
-
 m.directive("featureLayer", function ($q, esriRegistry) {
   return {
     restrict: "E",
---
+++ js/teamdev.esri.offline.js
@@ -0,0 +1,37 @@
+/* global O */
+/* global angular */
+var m = angular.module("teamdev.esri")
+.directive("offlineTileLayer", function ($q, esriRegistry) {
+  return {
+    restrict: "E",
+    require: "^esriMap",
+    replace: true,
+    scope: {
+      id: "@",
+      url: "@",
+      isOnLine: "=",
+      onReady: "&"
+    },
+    link: {
+      pre: function (scope, element, attrs, esriMap) {
+        var ready = $q.defer();
+        scope.isObjectReady = ready.promise;
+        require(["./lib/teamdev-esri-angularjs/js/libs/offline-tiles-advanced-min.js"], function () {
+          scope.this_layer = new O.esri.Tiles.OfflineTileEnablerLayer(scope.url, function () {
+            console.log("Offline Tile Layer Loaded.");
+            ready.resolve();
+
+            if (scope.onReady())
+              scope.onReady()(scope.this_layer);
+          }, scope.isOnLine);
+          esriMap.addLayer(scope.this_layer);
+
+          if (scope.id) {
+            scope.this_layer.id = scope.id;
+            esriRegistry.set(scope.id, scope.this_layer);
+          }
+        });
+      }
+    }
+  };
+});
